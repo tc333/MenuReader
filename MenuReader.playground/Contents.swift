8191647ea854c448da2eb5c1932e2e2f644d2b52
tsc333 <chamberlint@gmail.com>
2016-10-22 14:45:58 -0400

    Removed extension for XMLParserDelegate

--- MenuReader.playground/Contents.swift
+++ MenuReader.playground/Contents.swift
@@ -36,12 +36,10 @@ class Dish {
     }
 }
 
-class MenuReader: NSObject {
+class MenuReader: NSObject, XMLParserDelegate {
     
-    let menuURL = URL(string: "http://www.bates.edu/dining/menu/feed/todays-menu/")
-    
+    var menuURL: URL? = URL(string: "http://www.bates.edu/dining/menu/feed/todays-menu/")
     var parser: XMLParser?
-    
     var meals: [Meal] = []
     
     func initializeParser() {
@@ -54,10 +52,61 @@ class MenuReader: NSObject {
         parser?.parse()
     }
     
+    
+    func parser(_ parser: XMLParser, foundCDATA CDATABlock: Data) {
+        guard var CDATAString = String(data: CDATABlock, encoding: String.Encoding.utf8) else {
+            print("Cannot convert CDATABlock to string")
+            return
+        }
+        formatCDATAString(cDataString: &CDATAString)
+        parseMenuXMLWithCDATAString(CDATAString: CDATAString)
+    }
+    
     // MARK: - Helper Functions
     
-    func getHTMLTagValues(tag: String, fromString superString: String) -> [String]? {
-        guard let ranges = getRangesForHTMLTag(tag: tag, inSuperString: superString) else {
+    func parseMenuXMLWithCDATAString(CDATAString: String) {
+        if let mealExcerpts = getTagValues(tag: "meal", fromString: CDATAString) {
+            for mealExcerpt in mealExcerpts {
+                if let mealNames = getTagValues(tag: "h1", fromString: mealExcerpt) {
+                    for mealName in mealNames {
+                        let meal = Meal(name: mealName, stations: [])
+                        if let stationExcerpts = getTagValues(tag: "station", fromString: mealExcerpt) {
+                            for stationExcerpt in stationExcerpts {
+                                if let stationNames = getTagValues(tag: "h2", fromString: stationExcerpt) {
+                                    for stationName in stationNames {
+                                        let station = Station(name: stationName, dishes: [])
+                                        meal.stations.append(station)
+                                        if let disheNames = getTagValues(tag: "li", fromString: stationExcerpt) {
+                                            for dishName in disheNames {
+                                                let dish = Dish(name: dishName)
+                                                station.dishes.append(dish)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        self.meals.append(meal)
+                    }
+                }
+            }
+        }
+        print("First meal of the day is always the best: \(self.meals[0].name)")
+    }
+    
+    func formatCDATAString(cDataString: inout String) {
+        // Line breaks
+        cDataString = cDataString.replacingOccurrences(of: "\r", with: "")
+        // Ampersands
+        cDataString = cDataString.replacingOccurrences(of: "\u{26}amp;", with: "\u{26}")
+        // Single quotes
+        cDataString = cDataString.replacingOccurrences(of: "\u{26}quot;", with: "'")
+        // Apostrophe
+        cDataString = cDataString.replacingOccurrences(of: "\u{26}\u{23}039;", with: "'")
+    }
+    
+    func getTagValues(tag: String, fromString superString: String) -> [String]? {
+        guard let ranges = getRangesForTag(tag: tag, inSuperString: superString) else {
             print("Couldn't find given tag in the superString")
             return nil
         }
@@ -77,8 +126,8 @@ class MenuReader: NSObject {
         return returnArray
     }
     
-    // Searches a string for the given HTML tag and returns an array of NSRanges that describe the location(s) of the beginning and ending tag within the string
-    func getRangesForHTMLTag(tag: String, inSuperString superString: String) -> (beginningRanges: [NSRange], endingRanges: [NSRange])? {
+    // Searches a string for the given tag and returns an array of NSRanges that describe the location(s) of the beginning and ending tag within the string
+    func getRangesForTag(tag: String, inSuperString superString: String) -> (beginningRanges: [NSRange], endingRanges: [NSRange])? {
         
         let beginningTag = "<\(tag)>"
         let endingTag = "</\(tag)>"
@@ -100,59 +149,6 @@ class MenuReader: NSObject {
             print("Regular expression error: \(error.localizedDescription)")
             return nil
         }
-    }
-}
-
-extension MenuReader: XMLParserDelegate {
-    func parser(_ parser: XMLParser, foundCDATA CDATABlock: Data) {
-        guard var CDATAString = String(data: CDATABlock, encoding: String.Encoding.utf8) else {
-            print("Cannot convert CDATABlock to string")
-            return
-        }
-        formatCDATAString(cDataString: &CDATAString)
-        parseMenuXMLWithCDATAString(CDATAString: CDATAString)
-    }
-    
-    // MARK: - Helper Functions
-    
-    func parseMenuXMLWithCDATAString(CDATAString: String) {
-        if let mealExcerpts = getHTMLTagValues(tag: "meal", fromString: CDATAString) {
-            for mealExcerpt in mealExcerpts {
-                if let mealNames = getHTMLTagValues(tag: "h1", fromString: mealExcerpt) {
-                    for mealName in mealNames {
-                        let meal = Meal(name: mealName, stations: [])
-                        if let stationExcerpts = getHTMLTagValues(tag: "station", fromString: mealExcerpt) {
-                            for stationExcerpt in stationExcerpts {
-                                if let stationNames = getHTMLTagValues(tag: "h2", fromString: stationExcerpt) {
-                                    for stationName in stationNames {
-                                        let station = Station(name: stationName, dishes: [])
-                                        meal.stations.append(station)
-                                        if let disheNames = getHTMLTagValues(tag: "li", fromString: stationExcerpt) {
-                                            for dishName in disheNames {
-                                                let dish = Dish(name: dishName)
-                                                station.dishes.append(dish)
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        self.meals.append(meal)
-                    }
-                }
-            }
-        }
-    }
-    
-    func formatCDATAString(cDataString: inout String) {
-        // Line breaks
-        cDataString = cDataString.replacingOccurrences(of: "\r", with: "")
-        // Ampersands
-        cDataString = cDataString.replacingOccurrences(of: "\u{26}amp;", with: "\u{26}")
-        // Single quotes
-        cDataString = cDataString.replacingOccurrences(of: "\u{26}quot;", with: "'")
-        // Apostrophe
-        cDataString = cDataString.replacingOccurrences(of: "\u{26}\u{23}039;", with: "'")
     }
 }
 
